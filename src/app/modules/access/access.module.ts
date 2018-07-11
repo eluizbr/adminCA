0508c319d76d39456163fbed970f6be73e465908
Emerson Luiz <eluizbr@gmail.com>
2018-07-10 23:37:59 -0300

    Login module Redux ready

--- src/app/modules/access/access.module.ts
+++ src/app/modules/access/access.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { AccessRoutingModule } from './access-routing.module';
 import { LoginComponent } from './login/login.component';
+import { LoginEffects } from './store/login/login.effects';
+import { loginReducer } from './store/login/login.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
+    StoreModule.forFeature('login', loginReducer),
+    EffectsModule.forFeature([LoginEffects]),
     AccessRoutingModule
   ],
   declarations: [LoginComponent]
 })
-export class AccessModule { }
+export class AccessModule {}
